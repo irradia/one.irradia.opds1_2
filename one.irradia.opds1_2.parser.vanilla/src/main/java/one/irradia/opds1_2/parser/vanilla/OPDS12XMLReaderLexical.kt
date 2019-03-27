365befc96173b33a9764e59dc66210541b450f67
Mark Raynsford <code@io7m.com>
2019-03-27 11:21:55 +0000

    Break handler into inner class

--- one.irradia.opds1_2.parser.vanilla/src/main/java/one/irradia/opds1_2/parser/vanilla/OPDS12XMLReaderLexical.kt
+++ one.irradia.opds1_2.parser.vanilla/src/main/java/one/irradia/opds1_2/parser/vanilla/OPDS12XMLReaderLexical.kt
@@ -47,71 +47,76 @@ object OPDS12XMLReaderLexical {
     Preconditions.checkArgument(docBuilder.isNamespaceAware, "Document builder must be namespace aware")
     Preconditions.checkArgument(!docBuilder.isValidating, "Document builder must not be validating")
 
-    val document  = docBuilder.newDocument()
+    val document = docBuilder.newDocument()
     val elementStack = ArrayDeque<Element>()
     val textBuffer = StringBuilder()
-    val handler = object : DefaultHandler() {
-      private var locator: Locator? = null
-
-      override fun setDocumentLocator(locator: Locator) {
-        this.locator = locator
-      }
-
-      @Throws(SAXException::class)
-      override fun startElement(
-        uri: String,
-        localName: String,
-        qName: String,
-        attributes: Attributes) {
-
-        addTextIfNeeded()
-        val element = document.createElementNS(uri, qName)
-        for (i in 0 until attributes.length) {
-          element.setAttribute(attributes.getQName(i), attributes.getValue(i))
-        }
-
-        element.setUserData(
-          XML_TREE_LEXICAL_KEY,
-          one.irradia.opds1_2.lexical.OPDS12LexicalPosition(
-            source = source,
-            line = this.locator!!.lineNumber,
-            column = this.locator!!.columnNumber),
-          null)
-        elementStack.push(element)
-      }
-
-      override fun endElement(
-        uri: String,
-        localName: String,
-        qName: String) {
-
-        addTextIfNeeded()
-        val closedElement = elementStack.pop()
-        if (elementStack.isEmpty()) {
-          document.appendChild(closedElement)
-        } else {
-          val parentElement = elementStack.peek()
-          parentElement.appendChild(closedElement)
-        }
-      }
-
-      @Throws(SAXException::class)
-      override fun characters(ch: CharArray, start: Int, length: Int) {
-        textBuffer.append(ch, start, length)
-      }
-
-      private fun addTextIfNeeded() {
-        if (textBuffer.isNotEmpty()) {
-          val element = elementStack.peek()
-          val textNode = document.createTextNode(textBuffer.toString())
-          element.appendChild(textNode)
-          textBuffer.delete(0, textBuffer.length)
-        }
-      }
-    }
-
+    val handler = LexicalHandler(document, source, elementStack, textBuffer)
     parser.parse(inputStream, handler)
     return document
   }
 
+  internal class LexicalHandler(
+    private val document: Document,
+    private val source: URI,
+    private val elementStack: ArrayDeque<Element>,
+    private val textBuffer: StringBuilder
+  ) : DefaultHandler() {
+    private lateinit var locator: Locator
+
+    override fun setDocumentLocator(locator: Locator) {
+      this.locator = locator
+    }
+
+    @Throws(SAXException::class)
+    override fun startElement(
+      uri: String,
+      localName: String,
+      qName: String,
+      attributes: Attributes) {
+
+      this.addTextIfNeeded()
+      val element = this.document.createElementNS(uri, qName)
+      for (i in 0 until attributes.length) {
+        element.setAttribute(attributes.getQName(i), attributes.getValue(i))
+      }
+
+      element.setUserData(
+        XML_TREE_LEXICAL_KEY,
+        one.irradia.opds1_2.lexical.OPDS12LexicalPosition(
+          source = this.source,
+          line = this.locator.lineNumber,
+          column = this.locator.columnNumber),
+        null)
+      this.elementStack.push(element)
+    }
+
+    override fun endElement(
+      uri: String,
+      localName: String,
+      qName: String) {
+
+      this.addTextIfNeeded()
+      val closedElement = this.elementStack.pop()
+      if (this.elementStack.isEmpty()) {
+        this.document.appendChild(closedElement)
+      } else {
+        val parentElement = this.elementStack.peek()
+        parentElement.appendChild(closedElement)
+      }
+    }
+
+    @Throws(SAXException::class)
+    override fun characters(ch: CharArray, start: Int, length: Int) {
+      this.textBuffer.append(ch, start, length)
+    }
+
+    private fun addTextIfNeeded() {
+      if (this.textBuffer.isNotEmpty()) {
+        val element = this.elementStack.peek()
+        val textNode = this.document.createTextNode(this.textBuffer.toString())
+        element.appendChild(textNode)
+        this.textBuffer.delete(0, this.textBuffer.length)
+      }
+    }
+  }
 }
