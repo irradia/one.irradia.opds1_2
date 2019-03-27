c26bfd994ceceb56793b4696cfec53829c59ebd1
Mark Raynsford <code@io7m.com>
2019-03-27 11:01:11 +0000

    Add missing documentation annotations

--- one.irradia.opds1_2.commons/src/main/java/one/irradia/opds1_2/commons/OPDS12XMLParseError.kt
+++ one.irradia.opds1_2.commons/src/main/java/one/irradia/opds1_2/commons/OPDS12XMLParseError.kt
@@ -2,8 +2,33 @@ package one.irradia.opds1_2.commons
 
 import one.irradia.opds1_2.lexical.OPDS12LexicalPosition
 
+/**
+ * A specific parse error.
+ */
+
 data class OPDS12XMLParseError(
+
+  /**
+   * The parser that produced the error. This will typically be the name of the core parser, or
+   * the name of one of the extension parsers.
+   */
+
   val producer: String,
+
+  /**
+   * Lexical information for the parse error.
+   */
+
   val lexical: OPDS12LexicalPosition,
+
+  /**
+   * The error message.
+   */
+
   val message: String,
+
+  /**
+   * The exception raised, if any.
+   */
+
   val exception: Exception?)
