89e718ba796a4aad753f2357b54c0e57015e267b
Mark Raynsford <code@io7m.com>
2019-03-27 11:19:02 +0000

    Break into separate methods

--- one.irradia.opds1_2.nypl/src/main/java/one/irradia/opds1_2/nypl/OPDS12AvailabilityInference.kt
+++ one.irradia.opds1_2.nypl/src/main/java/one/irradia/opds1_2/nypl/OPDS12AvailabilityInference.kt
@@ -19,6 +19,22 @@ import org.w3c.dom.Element
 
 object OPDS12AvailabilityInference {
 
+  private fun mightBeHoldable(
+    xmlProcessor: OPDS12XMLProcessor,
+    copies: Element?,
+    acquisition: OPDS12Acquisition): OPDS12Availability {
+
+    return if (copies != null) {
+      val copiesAvailable =
+        xmlProcessor.optionalAttributeInt(copies, "available") ?: 0
+      if (copiesAvailable > 0)
+        OPDS12Availability.Loanable(acquisition)
+      else
+        OPDS12Availability.Holdable(acquisition)
+    } else
+      OPDS12Availability.Loanable(acquisition)
+  }
+
   /**
    * Determine the availability of the given element.
    */
@@ -35,48 +51,25 @@ object OPDS12AvailabilityInference {
     val copies =
       xmlProcessor.firstChildElementWithName(element, OPDS_URI, "copies")
 
-    fun mightBeHoldable(): OPDS12Availability {
-      return if (copies != null) {
-        val copiesAvailable =
-          xmlProcessor.optionalAttributeInt(copies, "available") ?: 0
-        if (copiesAvailable > 0)
-          OPDS12Availability.Loanable(acquisition)
-        else
-          OPDS12Availability.Holdable(acquisition)
-      } else
-        OPDS12Availability.Loanable(acquisition)
-    }
-
     /*
      * If there is no availability element, then the availability determination
      * has to be made purely upon the acquisition relation.
      */
 
-    if (availability == null) {
-      return when (acquisition.relation) {
-        ACQUISITION_BORROW ->
-          mightBeHoldable()
-
-        ACQUISITION_BUY ->
-          OPDS12Availability.Loanable(acquisition)
-
-        ACQUISITION_GENERIC ->
-          OPDS12Availability.Loaned(
-            acquisition = acquisition,
-            startDate = null,
-            endDate = null,
-            revokeURI = null)
-
-        ACQUISITION_OPEN_ACCESS ->
-          OPDS12Availability.OpenAccess(acquisition)
-
-        ACQUISITION_SAMPLE ->
-          OPDS12Availability.Loanable(acquisition)
-
-        ACQUISITION_SUBSCRIBE ->
-          OPDS12Availability.Loanable(acquisition)
-      }
+    return if (availability == null) {
+      withoutAvailability(acquisition, xmlProcessor, copies)
+    } else {
+      withAvailability(holds, xmlProcessor, availability, acquisition, copies)
     }
+  }
+
+  private fun withAvailability(
+    holds: Element?,
+    xmlProcessor: OPDS12XMLProcessor,
+    availability: Element,
+    acquisition: OPDS12Acquisition,
+    copies: Element?
+  ): OPDS12Availability {
 
     val position =
       holds?.let { xmlProcessor.optionalAttributeInt(holds, "position") }
@@ -129,8 +122,37 @@ object OPDS12AvailabilityInference {
           revokeURI = null)
 
       else ->
-        mightBeHoldable()
+        mightBeHoldable(xmlProcessor, copies, acquisition)
     }
   }
 
+  private fun withoutAvailability(
+    acquisition: OPDS12Acquisition,
+    xmlProcessor: OPDS12XMLProcessor,
+    copies: Element?): OPDS12Availability {
+
+    return when (acquisition.relation) {
+      ACQUISITION_BORROW ->
+        mightBeHoldable(xmlProcessor, copies, acquisition)
+
+      ACQUISITION_BUY ->
+        OPDS12Availability.Loanable(acquisition)
+
+      ACQUISITION_GENERIC ->
+        OPDS12Availability.Loaned(
+          acquisition = acquisition,
+          startDate = null,
+          endDate = null,
+          revokeURI = null)
+
+      ACQUISITION_OPEN_ACCESS ->
+        OPDS12Availability.OpenAccess(acquisition)
+
+      ACQUISITION_SAMPLE ->
+        OPDS12Availability.Loanable(acquisition)
+
+      ACQUISITION_SUBSCRIBE ->
+        OPDS12Availability.Loanable(acquisition)
+    }
+  }
 }
